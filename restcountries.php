332864c61b0587c6ec3a9eb0bcb97c15ab71768b
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2020-03-16 19:20:14 -0500

    misc enh + fixes

--- restcountries.php
+++ restcountries.php
@@ -1,10 +1,44 @@
 <?php
 
     // get POST data    
-    $search = $_POST['search'];   
-    $useExact = isset( $_POST['fullName']);
+    $search = $_POST['search'];                 // search text
+    $useExact = isset( $_POST['fullName']);     // exact match on name?
+    $searchBy = $_POST['searchType'];           // 1 = name lookup, 2 = country code lookup
 
 
+/*--------------------------------------------------------------------------------------------------
+
+notes - oddness / irregularities / comments on API behavior:
+
+- using 'exact match' will match against 2-digit country code, even when doing a 'name' search.
+    Not a problem, but seems somewhat incorrect.
+
+- search is not case-sensitive
+
+- some search terms return results that aren't an actual name match.  It appears that the API also
+    matches against the "altSpellings" field - without regard for whether I want it to.
+
+- country code search:  sending anything other than 2 or 3 characters will result in 
+    {"status":400,"message":"Bad Request"}
+
+- for either type of search, when no results are found, the API returns:
+    {"status":404,"message":"Not Found"}
+
+- when performing a name lookup, results are always returned as an array of objects, even when
+    there is only one result.  By contrast, when performing a country code lookup - which will 
+    only ever return 1 result - that result is sent as a single object, and NOT as an array.
+    That requires special handling below.
+
+- it seems like using '?fullText=true' and ?fields filtering interfere with each other...
+  so I am doing one or the other but not both.
+
+---------------------------------------------------------------------------------------------------*/
+
+
+    // --------------------------------------------------------------------------
+    // query URL construction
+
+    
     // field names we need to retrieve (API filter)
     $fields = array(
         "name", 
@@ -16,42 +50,76 @@
         "population", 
         "languages"
     );
-
-
-
-    $baseurl = 'https://restcountries.eu/rest/v2/name/';
     
+    // from form: 1 = search by name (api path "name");  2 = search by country code (api path "alpha")
+    $searchField = ($searchBy == "1") ? "/name/" : "/alpha/";
 
-    // oddness (possible issues with the API itself):
-    // - using 'exact match' will match against 2-digit country code, even when doing a 'name' search.
-    // 
+    $baseurl = 'https://restcountries.eu/rest/v2' . $searchField;
 
-    // it seems like using 'fullText=true' and field filtering interfere with each other.
-    // so I will use one or the other but not both.
+
     if ($useExact) {
+        // exact name match.  Omit field filtering for this query (see above)
         $fullURL = $baseurl . $search . "?fullText=true";
 
     } else {
-        $fullURL = $baseurl . $search . "?fields=" . implode(";", $fields);
+        // partial match.  include field filtering here
+        $fullURL = $baseurl . $search . "?fields=" . implode(";", $fields);   // like array.join
     }
 
-    $curl = curl_init($fullURL);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);  // prevent extra "1" in return value
 
-    $json = curl_exec($curl); // execute the API query
+
+
+    // --------------------------------------------------------------------------
+    // make cURL call to query API
+
+    // init cURL session
+    $curl = curl_init($fullURL);
+
+    // this prevents an unexpected "1" in api return value
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);  
+
+    // execute the query & close
+    $json = curl_exec($curl); 
     curl_close($curl);
 
 
+
+    // --------------------------------------------------------------------------
+    // results handling / processing
+
     // convert json to array
-    $data = json_decode($json);   
+    $data = json_decode($json);
 
-    // if no matches found, the API returns {"status":404, "message":"Not Found"}
-    // test for existence of "status" key - and if so, send that up directly as the result.
-    if (array_key_exists('status', $data)) {   
 
-        echo $json;
+    // check for error condition or no data returned
+    if (array_key_exists('status', $data)) {
+
+        $errCode = $data->status;
+        $data->error = true;    // add this flag for easier client-side identification of error condition
+
+        if ($errCode == 400) {
+            $data->errMsg = "Invalid request - please try again.";
+
+        } elseif ($errCode == 404) {
+            $data->errMsg = "Your search returned no results - please try again.";
+
+        } else {
+            $data->errMsg = "An unknown error occurred - please try again.";
+        };
+
+
+        echo json_encode($data);
+
 
     } else {    // normal handling
+
+        // country code search returns one object, NOT wrapped in an array.
+        // whereas lookup-by-name always returns an array of objects.
+
+        // easiest to just convert to array if needed:
+        if (!is_array($data)) {
+            $data = array($data);
+        }
 
         // sort alpha ascending on name
         usort($data, function ($a, $b) {return strcmp($a->name, $b->name);} );
@@ -63,7 +131,6 @@
         echo json_encode($data);  
 
     };
-
 
 
 ?>
--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
-
+"use strict";
 
 const init = () => {
-
 
     const form = document.getElementById("theForm");
     const rsltDiv = document.getElementById("results");
     
+    // initialize focus on text input field
+    document.getElementById("search").focus();
+
 
     // =======================================================================
     // form "submit" handling
@@ -14,23 +16,35 @@ const init = () => {
 
         evt.preventDefault();       // cancel actual form submission
 
+        // perform client-side form validation prior to submitting to server
+
         const formData = new FormData(form);    
+        const srch = formData.get("search");
 
-
-        const srch = formData.get('search');
+        // check for blank search field
         if (!srch) {
-            showResults({"status": 404, "message": "Please enter a search term."});
-            //alert("search text required!");
-            //return false;
+            showErrorResult("Please enter a search term.");
+            return;         // get out
         }
-                
+
+        // check for valid country code search entry (2 or 3 characters required)
+        if (formData.get("searchType") == 2) { 
+            if (srch.length < 2 || srch.length > 3) {
+                showErrorResult("Country code search must be 2 or 3 characters long.");
+                return;     // get out
+            }
+        }
+
+        // run the search
         runSearch(formData)
         .then( json => showResults(json));
 
     })
     
+
     // =======================================================================
     // perform the backend call (php -> REST API) to run the search
+    // uses async/await - returns a promise
     //
     async function runSearch(formData)  {
 
@@ -40,41 +54,71 @@ const init = () => {
         });
 
         const rslt = await resp.json();
-        console.log(rslt);
         return rslt;
     }
+
+
     
-    // build out the display with the API json results
-    const showResults = results => {
-        
-        // clear the results div
+    const clearResults = () => {
+        // clear any prior results / state
+        rsltDiv.classList.remove("errorState");
         while (rsltDiv.firstChild) {
             rsltDiv.firstChild.remove();
         }
 
-        if (results.status) {
-            // no matches returned from api
-            rsltDiv.textContent = results.message;
+    }
+    
+    const showErrorResult = errText => {
+        clearResults();
+        rsltDiv.classList.add("errorState");
+        rsltDiv.textContent = errText;
+    }
+
+
+
+
+    // =======================================================================
+    // build out the display with the API json results
+    //
+    const showResults = results => {
+        
+
+        // handle error conditions
+        if (results.error) {        
+            showErrorResult(results.errMsg);
+            return;
+
 
         } else {
-            // normal case - 1 or more results
-            // results is an array of objects...
+
+            clearResults();
+
+            const nf = new Intl.NumberFormat();  // use for thousands separators below
+            let countryCount = 0, regCount = {}, subCount = {};
+
             results.forEach( obj => {
+
                 let div = document.createElement("div");
-                div.classList.add("country", "border");
+                div.classList.add("country");
                 
                 // destructuring assignment
                 let { name, alpha2Code, alpha3Code, flag, region, subregion, population, languages } = obj;
 
                 languages = languages.map( i => i.name).join(", ");
+
+                // aggregate summary data
+                countryCount++;
+                (regCount[region]) ? regCount[region]++ : regCount[region] = 1;
+                (subCount[subregion]) ? subCount[subregion]++ : subCount[subregion] = 1; 
                 
+
                 div.innerHTML =  `
                     <div class='flex flex2'>
                         <img src=${flag} />
                     </div>
                     <div class='flex flex4'>
                         <p><span class='label'>Country:</span> ${name}</p>
-                        <p><span class='label'>Population:</span> ${Intl.NumberFormat().format(population)}</p>
+                        <p><span class='label'>Population:</span> ${nf.format(population)}</p>
                         <p><span class='label'>Languages:</span> ${languages}</p>
                     </div>
                     <div class='flex flex4'>
@@ -84,15 +128,26 @@ const init = () => {
                     </div>
                 `;
 
-                
-
-
-
                 rsltDiv.appendChild(div);
-            });
-    
-        }
 
+            });
+
+
+            document.getElementById("result-count").textContent = countryCount;
+
+            let str = "";
+            for (let key in regCount) {
+                str += `${key} (${regCount[key]})  |  `;
+            }
+            document.getElementById("region-list").textContent = str;
+
+            str = "";
+            for (let key in subCount) {
+                str += `${key} (${subCount[key]})  |  `;
+            }
+            document.getElementById("subregion-list").textContent = str;
+
+        }
 
     }
 
